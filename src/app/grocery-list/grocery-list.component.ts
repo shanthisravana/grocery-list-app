b224b03246c4aabf1b31b4abf610f5398b52107a
shanthisravana <shanthi.sravana@gmail.com>
2020-09-02 12:12:40 -0700

    Added onEdit and onDelete

--- src/app/grocery-list/grocery-list.component.ts
+++ src/app/grocery-list/grocery-list.component.ts
@@ -5,9 +5,13 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './grocery-list.component.html',
   styleUrls: ['../../assets/css/bootstrap.min.css']
 })
+
 export class GroceryListComponent implements OnInit {
 
-  task: string;
+  task = {
+    id: 0,
+    name: ''
+  };
   tasks = [];
 
   constructor() { }
@@ -16,12 +20,44 @@ export class GroceryListComponent implements OnInit {
   }
 
   onClick(){
-    this.tasks.push({name: this.task});
-    console.log(this.task);
-    this.task = '';
-  }
   
 
+    if (this.task.id == 0){
+      this.tasks.push({id: (new Date()).getTime(), name: this.task.name, strike: false});
+    }
+
+    this.task = {
+      name: '',
+      id: 0
+    };
+  }
+
+  onEdit(item){
+    this.task = item;
+  }
+  
+  onDelete(item){
+    for(var i=0; i<this.tasks.length; i++){
+      if(item.id == this.tasks[i].id){
+        this.tasks.splice(i,1);
+        break;
+      }
+    }
+  }
+
+  onStrike(item){
+    for(var i=0; i<this.tasks.length; i++){
+      if(item.id == this.tasks[i].id){
+        if(this.tasks[i].strike){
+          this.tasks[i].strike = false;
+        }
+        else{
+          this.tasks[i].strike = true;
+        }
+        break;
+      }
+    }
+  }
   
 
 }
